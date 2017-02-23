20a4aa11384a906e36d2c1a56cc74c14e2f71a3f
Andrey Kim <drewkenjo@users.noreply.github.com>
2017-02-23 13:32:34 -0500

    moved header to src folder and modified sconscript to compile src directory

---
+++ src/feio.h
@@ -0,0 +1,73 @@
+#ifndef feio_hh
+#define feio_hh
+
+
+namespace RICHfrontend{
+ enum{	NCHANNELS = 192,
+		MAXEDGES = 2000};
+
+ struct rawEvent{
+	Int_t trigID=0;
+	ULong64_t timeStamp=0;
+	UChar_t hold;
+
+	UShort_t fadc[NCHANNELS];
+
+	UShort_t fnedge;
+	UShort_t ftdc[MAXEDGES];
+	UShort_t fchan[MAXEDGES];
+	Bool_t fpolar[MAXEDGES];
+ };
+
+
+ class RICHEvent{
+  protected:
+	bool fLeadingEdge = true;
+	bool fFallingEdge = false;
+
+	ULong64_t timeStamp;
+	Int_t trigID, hold;
+	UShort_t fadc[NCHANNELS];
+
+	std::vector<UShort_t> ftdc[NCHANNELS];
+	std::vector<Bool_t> fpolar[NCHANNELS];
+
+	std::vector<UShort_t> ftime[NCHANNELS];
+	std::vector<UShort_t> fdur[NCHANNELS];
+
+  public:
+	RICHEvent(){};
+
+	void Clear(){
+		trigID=0;
+		timeStamp=0;
+
+		for(int ich=0;ich<NCHANNELS;ich++){
+			ftdc[ich].clear();
+			fpolar[ich].clear();
+			ftime[ich].clear();
+			fdur[ich].clear();
+		}
+	};
+
+	void Fill(rawEvent &rev){
+		Clear();
+
+		trigID = rev.trigID;
+		timeStamp = rev.timeStamp;
+		hold = rev.hold;
+
+		std::copy(rev.fadc, rev.fadc+NCHANNELS, fadc);
+
+		for(int iedge=0; iedge<rev.fnedge; iedge++){
+			int ichan = rev.fchan[iedge];
+			fpolar[ichan].push_back(rev.fpolar[iedge]);
+			ftdc[ichan].push_back(rev.ftdc[iedge]);
+		}
+	};
+
+	~RICHEvent(){};
+ };
+}
+
+#endif
