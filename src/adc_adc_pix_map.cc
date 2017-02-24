dd7dc717546e836a40d8ef48314592b4b8ca63d0
Andrey Kim <drewkenjo@users.noreply.github.com>
2017-02-24 18:29:32 -0500

    save map to tree

--- src/adc_adc_pix_map.cc
+++ src/adc_adc_pix_map.cc
@@ -6,26 +6,28 @@
 #include<TTree.h>
 #include<TString.h>
 #include<TRegexp.h>
-#include<TStyle.h>
 #include"feio.h"
 
 using namespace RICHfrontend;
 
 class goodRICHEvent:public RICHEvent{
   public:
-	goodRICHEvent(int);
+	goodRICHEvent(TString);
 	~goodRICHEvent();
 	void Fill(rawEvent&);
 
   private:
-	unsigned char ipix[64];
 	int nasic;
+	TString fname;
 	boost::container::flat_map<uint32_t, uint32_t> hh[NCHANNELS];
+	uint8_t ipix[64];
 };
 
 
-goodRICHEvent::goodRICHEvent(int _nasic):nasic(_nasic){
-	unsigned char ipixels[] = {60, 58, 59, 57, 52, 50, 51, 49, 44, 42, 43, 41, 36, 34, 35, 33, 28, 26, 27, 25, 20, 18, 19, 17, 12, 10, 11, 9, 4, 2, 3, 1, 5, 7, 6, 8, 13, 15, 14, 16, 21, 23, 22, 24, 29, 31, 30, 32, 37, 39, 38, 40, 45, 47, 46, 48, 53, 55, 54, 56, 61, 63, 62, 64};
+goodRICHEvent::goodRICHEvent(TString _fname):fname(_fname)
+{
+	nasic = fname.Contains("2ASIC") ? 2 : 3;
+	uint8_t ipixels[] = {60, 58, 59, 57, 52, 50, 51, 49, 44, 42, 43, 41, 36, 34, 35, 33, 28, 26, 27, 25, 20, 18, 19, 17, 12, 10, 11, 9, 4, 2, 3, 1, 5, 7, 6, 8, 13, 15, 14, 16, 21, 23, 22, 24, 29, 31, 30, 32, 37, 39, 38, 40, 45, 47, 46, 48, 53, 55, 54, 56, 61, 63, 62, 64};
 	std::copy(ipixels, ipixels+64, ipix);
 }
 
@@ -46,6 +48,25 @@ void goodRICHEvent::Fill(rawEvent &rev){
 
 
 goodRICHEvent::~goodRICHEvent(){
+	TFile* ff = new TFile(Form("adcPixMap_%s", basename(fname.Data())), "recreate");
+	TTree* tt = new TTree("tmap", "adc pixel mapping");
+
+	UInt_t key, val;
+	UChar_t chan;
+	tt->Branch("chan", &chan, "chan/b");
+	tt->Branch("key", &key, "key/i");
+	tt->Branch("val", &val, "val/i");
+
+	for(int chan=0;chan<NCHANNELS;chan++)
+	for (boost::container::flat_map<uint32_t, uint32_t>::iterator it=hh[chan].begin(); it!=hh[chan].end(); ++it){
+		key = it->first;
+		val = it->second;
+		tt->Fill();
+	}
+
+	ff->Write();
+
+	delete tt, ff;
 }
 
 
@@ -53,7 +74,7 @@ goodRICHEvent::~goodRICHEvent(){
 int main(int argc, char** argv)
 {
  rawEvent rawEv;
- goodRICHEvent ev(TString(argv[1]).Contains("2ASIC") ? 2 : 3);
+ goodRICHEvent ev(argv[1]);
 
  for(int iarg=1;iarg<argc;iarg++){
 	if(!TString(argv[iarg]).Contains(TRegexp(".root$"))) continue;
@@ -66,7 +87,6 @@ int main(int argc, char** argv)
 
 	int nen = tt->GetEntries();
 	for(int ien=0; ien<nen; ien++){
-		if(ien%(nen/100)==0) std::cout<<ien*100.1/nen<<std::endl;
 		tt->GetEntry(ien);
 		ev.Fill(rawEv);
 	}
