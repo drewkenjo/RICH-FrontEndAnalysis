7ab417fa2cd2d8eac224236038ca347761aa45e5
Andrey Kim <drewkenjo@users.noreply.github.com>
2017-02-26 05:08:32 -0500

    include channels from other asics to correlations

--- src/adc_adc_pix_map.cc
+++ src/adc_adc_pix_map.cc
@@ -20,15 +20,12 @@ class goodRICHEvent:public RICHEvent{
 	int nasic;
 	TString fname;
 	boost::container::flat_map<uint32_t, uint32_t> hh[NCHANNELS];
-	uint8_t ipix[64];
 };
 
 
 goodRICHEvent::goodRICHEvent(TString _fname):fname(_fname)
 {
 	nasic = fname.Contains("2ASIC") ? 2 : 3;
-	uint8_t ipixels[] = {60, 58, 59, 57, 52, 50, 51, 49, 44, 42, 43, 41, 36, 34, 35, 33, 28, 26, 27, 25, 20, 18, 19, 17, 12, 10, 11, 9, 4, 2, 3, 1, 5, 7, 6, 8, 13, 15, 14, 16, 21, 23, 22, 24, 29, 31, 30, 32, 37, 39, 38, 40, 45, 47, 46, 48, 53, 55, 54, 56, 61, 63, 62, 64};
-	std::copy(ipixels, ipixels+64, ipix);
 }
 
 
@@ -37,12 +34,13 @@ void goodRICHEvent::Fill(rawEvent &rev){
 
 	for(int ichan=0; ichan<NCHANNELS; ichan++)
 	{
-		int iasic = ichan/64;
-		for(int ich=iasic*64;ich<(iasic+1)*64;ich++){
-			uint32_t key = (ipix[ich%64]<<24) | (fadc[ichan]<<12) | (fadc[ich]);
+		for(int ich=0;ich<NCHANNELS;ich++){
+			uint32_t key = (ich<<24) | (fadc[ichan]<<12) | (fadc[ich]);
 			hh[ichan][key]++;
+
+			if(nasic==2 && ich==63) ich += 64;
 		}
-		if(nasic==2 && ichan==63) ichan+=64;
+		if(nasic==2 && ichan==63) ichan += 64;
 	}
 }
 
